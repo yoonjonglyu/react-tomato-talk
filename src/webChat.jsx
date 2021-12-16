5080b27b4abdd0f75aa9b9ccf080d9008958d702
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-12-16 20:39:43 +0900

    feat: WEBCHAT - 서버 연결 상태 안내 UI 연결

--- src/webChat.jsx
+++ src/webChat.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from 'react';
 
 import ChatWindow from './talk/chatWindow';
-
+import Loading from './loading';
 
 const WebChat = (props) => {
     const {
         socket
     } = props;
-
     const [step, setStep] = useState(0);
     const handleStep = (step) => {
         setStep(step);
@@ -20,7 +19,14 @@ const WebChat = (props) => {
                     socketIdx: socket.id,
                     room: '#1'
                 });
+                handleStep(2);
             }
+        });
+        socket.on('disconnect', () => {
+            handleStep(1);
+        });
+        socket.on('connect_error', () => {
+            handleStep(1);
         });
         return () => {
             socket.close();
@@ -34,14 +40,15 @@ const WebChat = (props) => {
             flexFlow: "column wrap",
             flex: "1",
             justifyContent: "center",
+            position: "relative",
             border: "1px solid #678983",
         }}
         >
-
             {
-                step === 0 &&
-                <ChatWindow socket={socket} />
+                step < 2 &&
+                <Loading state={step} />
             }
+            <ChatWindow socket={socket} />
         </article >
     );
 }
