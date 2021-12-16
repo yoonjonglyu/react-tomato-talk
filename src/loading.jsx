7ea362a2dc8976e0b3e8ebe2ad4d3a29ecf10ed8
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-12-16 20:37:22 +0900

    feat: LOADING - 소켓 연결 상태 안내 UI

---
+++ src/loading.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Loading = ({ state }) => {
+    const message = [
+        "연결중입니다.",
+        "채팅 서버 상태를 확인해주세요."
+    ];
+
+    return (
+        <div
+            style={{
+                display: "flex",
+                flexFlow: "column",
+                justifyContent: "center",
+                position: "absolute",
+                width: "100%",
+                height: "100%",
+                background: "rgb(60, 60, 60, 50%)",
+            }}
+        >
+            <h2
+                style={{
+                    textAlign: "center",
+                    fontSize: "0.9rem",
+                    fontWeight: "normal",
+                    color: "tomato",
+                    textShadow: "0.5px 0.5px #88473b",
+                }}
+            >
+                {message[state]}
+            </h2>
+        </div>
+    );
+}
+
+export default Loading;
