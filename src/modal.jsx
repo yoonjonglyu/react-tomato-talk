d3f5f817f75edb5493aa14b5f82e905a0341a5c7
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-12-25 23:13:38 +0900

    feat: MODAL - INIT

---
+++ src/modal.jsx
@@ -0,0 +1,42 @@
+import React, { useContext } from 'react';
+
+import { ModalContext } from './store/modalContext';
+
+const Modal = () => {
+    const { handleIsModal, modalContents } = useContext(ModalContext);
+    const closeModal = () => {
+        handleIsModal(false);
+    }
+
+    return (
+        <article
+            style={{
+                position: "absolute",
+                display: "flex",
+                flexFlow: "column",
+                justifyContent: "center",
+                width: "100%",
+                height: "100%",
+                background: "rgb(0,0,0, 0.5)",
+            }}
+            onClick={closeModal}
+        >
+            <div data-testid="modal-box"
+                style={{
+                    display: "flex",
+                    flexFlow: "column wrap",
+                    justifyContent: "center",
+                    minWidth: "50%",
+                    minHeight: "30%",
+                    margin: "0 auto",
+                    background: "#bb5f5f",
+                    borderRadius: "3px",
+                }}
+            >
+                {modalContents}
+            </div>
+        </article>
+    );
+};
+
+export default Modal;
