a12b4a3d3daf30b68136df4b789e64912816740a
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-01-23 22:42:07 +0900

    feat(CHATROOM): USER MESSAGE - 이미지 다운로드

--- src/talk/chatRoom/userMessage.jsx
+++ src/talk/chatRoom/userMessage.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const UserMessage = ({ message, openImageModal, time }) => {
+    const [isHover, setIsHover] = useState(false);
+
+    const downloadImage = (image) => {
+        const imageLink = document.createElement("a");
+        imageLink.href = image;
+        imageLink.download = `이미지_${Date.now()}.png`;
+        imageLink.click();
+        imageLink.remove();
+    }
+
     const messageStyle = {
         maxWidth: "58%",
         padding: "8px",
@@ -12,6 +22,7 @@ const UserMessage = ({ message, openImageModal, time }) => {
         borderRadius: "6px",
     };
     const boxStyle = {
+        position: "relative",
         display: "flex",
         flex: "1",
         flexFlow: "row-reverse",
@@ -31,13 +42,36 @@ const UserMessage = ({ message, openImageModal, time }) => {
                         >
                             {message}
                         </span > :
+                        <>
                         <img
                             data-testid="user-image"
                             src={message.slice(5)}
                             style={messageStyle}
                             onClick={() => openImageModal(message.slice(5))}
+                            onMouseEnter={() => setIsHover(true)}
+                            onMouseLeave={() => setIsHover(false)}
                             alt="전송된 이미지"
                         />
+                        {
+                            isHover &&
+                            <button
+                                style={{
+                                    position: "absolute",
+                                    margin: "8px",
+                                    padding: "3px",
+                                    fontSize: "0.8rem",
+                                    color: "#cecece",
+                                    background: "rgb(255, 99, 71, 50%)",
+                                    border: "none",
+                                }}
+                                onMouseEnter={() => setIsHover(true)}
+                                onMouseLeave={() => setIsHover(false)}
+                                onClick={() => downloadImage(message.slice(5))}
+                            >
+                                Download
+                            </button>
+                        }
+                    </>
                 }
                 <time
                     style={{
